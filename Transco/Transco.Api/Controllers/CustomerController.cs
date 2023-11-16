efaba8bce571830dbc7ad4813787b3245e156d7c
positibo <markpositibo.deleon@gmail.com>
2023-11-16 19:40:33 +0800

    sample project

---
+++ Transco/Transco.Api/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Transco.Data;
+
+namespace Transco.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public CustomerController(DataContext dataContext) => this._dataContext = dataContext;
+
+
+        [HttpGet(Name = "GetCustomers")]
+        public IEnumerable<Customer> Get()
+        {
+            return _dataContext.Customers.ToList();
+        }
+    }
+}
---
+++ Transco/Transco.Data/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Transco.Data;
+
+public partial class Customer
+{
+    public decimal Customerid { get; set; }
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
